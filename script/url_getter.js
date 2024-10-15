d5b969c64781fa4b8f841303306bc78b0eddb839
DODOPPPLER <131638292+DODOPPPLER@users.noreply.github.com>
2024-10-15 15:45:29 +0000

    3

--- script/url_getter.js
+++ script/url_getter.js
@@ -6,27 +6,15 @@ document.addEventListener('DOMContentLoaded', function() {
     const match = pdf_URL.match(regex);
     var pdfName = '';
 
-    
-    if (pdf_URL) {
+    pdfName = match[1];
 
-        if (match) {
 
-            pdfName = match[1];
-    
-        }   else {
-    
-            pdfName = 'no se encontró el nombre del archivo';
-    
-        }
+    var googleViewer = 'https://docs.google.com/gview?url=' + pdf_URL + '&embedded=true';
+    // Inserción de datos a HTML
+    document.getElementById("pdfNameH").innerText = pdfName;
+    document.getElementById("pdfNameT").innerText = pdfName;
+    document.getElementById("miIframe").src = googleViewer;
 
-        var googleViewer = 'https://docs.google.com/gview?url=' + pdf_URL + '&embedded=true';
-        // Inserción de datos a HTML
-        document.getElementById("pdfNameH").innerText = pdfName;
-        document.getElementById("pdfNameT").innerText = pdfName;
-        document.getElementById("miIframe").src = googleViewer;
-    } else {
-        console.error("El parámetro 'pdf_URL' no está presente en la URL.");
-    }
 
 });
 
